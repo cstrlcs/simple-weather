bf988f9c1ec87c458be291bdbefc97167b56a257
Lucas Castro <castroclucas@gmail.com>
2023-11-17 12:07:30 -0300

    feat: first commit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { config } from 'dotenv';
 if (process.env.NODE_ENV !== 'production') {
   config();
 }
-// call after config() to access the env variables
 import { app } from './api';
 
 const port = process.env.PORT || 3333;
